678f07b317e52e653d7a1e9f0543f756fa6e16f9
dop3file <49581209+dop3file@users.noreply.github.com>
2022-12-18 12:49:05 +0300

    update

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+var technologies = ['python', 'postgresql', 'django + drf', 'fastapi', 'flask', 'aiogram', 'redis', 'celery'];
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function includeText() {
+    var text = "<b>Stack:</b>";
+
+    const div = document.createElement('div');
+    div.className = 'row'
+
+    div.innerHTML += '<b>Stack </b>'
+
+    for (const tech in technologies) {
+        div.innerHTML += `<span style="background-color: ${getRandomColor()};" class="label" id="label${tech}" onclick="changeColor()">${technologies[tech]}</span>`;
+    }
+
+    var element = document.getElementById("stack");
+    element.appendChild(div);
+}
+
+function changeColor() {
+    for (const i in technologies) {
+        document.getElementById("label" + String(i)).style.backgroundColor = getRandomColor();
+    }
+}
+
+
+includeText()
+
+
+
---
+++ scripts/script_projects.js
@@ -0,0 +1,70 @@
+var projects = ['Шахматы', 'Социальная сеть', 'API Гомельского транспорта', 'Язык программирования', 'Анонимный чат бот']
+var images = [
+    'chess.png',
+    'socialnetwork.png',
+    'api.png',
+    'eq.jfif',
+    'anonym.png'
+]
+
+var projects = {
+    0: {
+        name: 'Шахматы',
+        description: 'Шахматный движок написанный на Python с пользовательским интерфейсом на PyGame и мультиплеером на socket Из возможностей интерфейса: подсветка возможных ходов количество ходов таймер с временем выбывшие фигуры переворот доски Также в движке присутствуют шахматные боты 3 уровней',
+        image: 'chess.png',
+        github: 'https://github.com/dop3file/chess'
+    },
+    1: {
+        name: 'Социальная сеть',
+        description: 'mood - социальная сеть на Django\nИз реализованного функционала :\nпрофиль\nизменения профиля\nинтересы\nпосты\nуведомления\nлента\nсохранненые посты\nлайки\nподписки и т.д',
+        image: 'socialnetwork.png',
+        github: 'https://github.com/dop3file/mood_social_network'
+    },
+    2: {
+        name: 'API Гомельского транспорта',
+        description: 'JSON API для получения информации о гомельском общественном транспорте',
+        image: 'api.png',
+        github: 'https://github.com/dop3file/gomeltrans_api'
+    },
+    3: {
+        name: 'Язык программирования',
+        description: 'Equilibrium - это скриптовый строго типизированный язык программирования, написанный на Python',
+        image: 'eq.jfif',
+        github: 'https://github.com/dop3file/equilibrium'
+    },
+    4: {
+        name: 'Анонимный чат бот',
+        description: 'Анонимный телеграм чат бот Chatium для анонимного общения',
+        image: 'anonym.png',
+        github: 'https://github.com/dop3file/anonym_chat'
+    }
+}
+
+function includeProjectText() {
+    all_projects_span = document.querySelectorAll('span.project_tab');
+    var parent = document.getElementById('nav__');
+
+    for(var i = 0; i < Object.keys(projects).length; i++) {
+        var span_tag = document.createElement('span');
+        span_tag.innerHTML = projects[i]['name'];
+        span_tag.className = 'project_tab';
+        span_tag.id = `project${i}`;
+        span_tag.setAttribute("onclick","getProject(this.id);");
+        parent.appendChild(span_tag);
+    }
+
+}
+
+function getProject(id) {
+    id = parseInt(id.slice(-1))
+    console.log()
+    description = document.getElementById('description');
+    description.innerHTML = projects[id]['description'];
+    image = document.getElementById('project_image')
+    image.src = 'images/projects/' + projects[id]['image'];
+    label = document.getElementById('project_label')
+    label.innerHTML = projects[id]['name'];
+  }
+
+
+includeProjectText()
